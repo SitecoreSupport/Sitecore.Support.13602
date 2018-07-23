ed9fa89d05d262c88221a57fb2e7be0ecf29b897
DK\IEKA <ieka@sitecore.net>
2018-07-23 18:23:51 +0300

    Fix link resolving when "Host Name" contains several values

---
+++ src/Sitecore.Support.13602/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.13602")]
+[assembly: AssemblyProduct("Sitecore.Support.13602")]
+[assembly: ComVisible(false)]
---
+++ src/Sitecore.Support.13602/XA/Foundation/Multisite/LinkManagers/LocalizableLinkBuilder.cs
@@ -0,0 +1,67 @@
+﻿using Sitecore.Links;
+using Sitecore.Sites;
+using Sitecore.Web;
+using System;
+
+namespace Sitecore.Support.XA.Foundation.Multisite.LinkManagers
+{
+  public class LocalizableLinkBuilder : LinkProvider.LinkBuilder
+  {
+    public LocalizableLinkBuilder(UrlOptions options)
+      : base(options)
+    {
+    }
+
+    protected override string GetServerUrlElement(SiteInfo siteInfo)
+    {
+      return GetServerUrlElement(siteInfo, this.AlwaysIncludeServerUrl);
+    }
+
+    public string GetServerUrlElement(SiteInfo siteInfo, bool alwaysIncludeServerUrl)
+    {
+      SiteContext site = Context.Site;
+      string str = (site != null) ? site.Name : string.Empty;
+      string str2 = alwaysIncludeServerUrl ? WebUtil.GetServerUrl() : string.Empty;
+      if ((siteInfo == null) || siteInfo.Name.Equals(str, StringComparison.OrdinalIgnoreCase))
+      {
+        return str2;
+      }
+      string hostName = WebUtil.GetHostName();
+      string[] values = new string[] { siteInfo.TargetHostName, IsHostNameUnique(siteInfo) ? siteInfo.HostName : null, hostName };
+      string str4 = StringUtil.GetString(values);
+      if ((str4 == string.Empty) || (str4.IndexOf('*') >= 0))
+      {
+        return str2;
+      }
+      if (str4.Contains("|"))
+      {
+        str4 = str4.Substring(0, str4.IndexOf('|'));
+      }
+      string scheme = WebUtil.GetScheme();
+      string[] textArray2 = new string[] { siteInfo.Scheme, scheme, "http" };
+      string str6 = StringUtil.GetString(textArray2);
+      if (str6 == string.Empty)
+      {
+        return str2;
+      }
+      int port = WebUtil.GetPort();
+      int @int = MainUtil.GetInt(siteInfo.Port, port);
+      if ((str4.Equals(hostName, StringComparison.OrdinalIgnoreCase) && (@int == port)) && str6.Equals(scheme, StringComparison.OrdinalIgnoreCase))
+      {
+        return str2;
+      }
+      string str7 = str6 + "://" + str4;
+      if ((@int > 0) && (@int != 80))
+      {
+        str7 = str7 + ":" + @int;
+      }
+      return str7;
+    }
+
+    public static bool IsHostNameUnique(SiteInfo siteInfo)
+    {
+      char[] anyOf = new char[] { '*' };
+      return (siteInfo.HostName.IndexOfAny(anyOf) < 0);
+    }
+  }
+}
